5e9412b2577ec583973b682c04a028219906fb91
cesarsr <cesarsrx@gmail.com>
2024-05-05 22:59:29 +1200

    Initialize git in API project

---
+++ core/user/tests/test_viewsets.py
@@ -0,0 +1,47 @@
+from rest_framework import status
+from core.fixtures.user import user
+from core.fixtures.post import post
+
+
+class TestUserViewSet:
+    endpoint = '/api/user/'
+    
+    ###########################################################################
+    ############### Test Comment ViewSet with authenticated user ##############
+    ###########################################################################
+    
+    def test_list(self, client, user):
+        client.force_authenticate(user=user)
+        response = client.get(self.endpoint)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 1
+        
+    def test_retrieve(self, client, user):
+        client.force_authenticate(user=user)
+        response = client.get(self.endpoint + str(user.public_id) + "/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id']        == user.public_id.hex
+        assert response.data['username']  == user.username
+        assert response.data['email']      == user.email
+        assert response.data['first_name'] == user.first_name
+        assert response.data['last_name']  == user.last_name
+        
+    def test_create(self, client, user):
+        client.force_authenticate(user=user)
+        
+        # We are expecting a 405 error, so the data will be an empty dict
+        
+        data = {}
+        
+        response = client.post(self.endpoint, data)
+        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+    def test_update(self, client, user):
+        client.force_authenticate(user=user)
+        data = {
+            "email": "johndoe@yopmail.com",
+        }
+        response = client.patch(self.endpoint + str(user.public_id) + "/", data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['email'] == data['email']
+
